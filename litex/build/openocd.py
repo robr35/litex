129f8c707e30dcd7e24642f1405037bbf765b6df
Rob Rawnsley <robrawnsley@gmail.com>
2024-08-03 23:50:32 +1200

    Add newline in OpenOCD jtagserver split to work with newer OpenOCD version

--- litex/build/openocd.py
+++ litex/build/openocd.py
@@ -121,7 +121,7 @@ proc jtagstream_poll {tap tx n} {
     set txi [concat {*}$txi]
 """
         cfg += f"""
-    set rxi [split [drscan $tap {{*}}$txi {endstate}] " "]
+    set rxi [split [drscan $tap {{*}}$txi {endstate}] " \n"]
 """
         cfg += """
     #echo $txi:$rxi
